6ce56f3f6dbef8da1022762612f4df2ad3059d25
pascalh <paahuijs@avans.nl>
2016-01-05 12:04:27 +0100

    script

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour {
+
+	public float maxSpeed;
+
+	private float currentSpeed;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
