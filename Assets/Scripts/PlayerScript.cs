3abf83cd12d99619b2d31887802e722816971250
pascalh <paahuijs@avans.nl>
2016-01-05 13:09:36 +0100

    movement player

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -3,24 +3,25 @@ using System.Collections;
 
 public class PlayerScript : MonoBehaviour {
 
-	public float maxSpeedKMH;
-	public float speedIncresement; 
+	public float maxSpeed;
+	public float tilt;
 
-	private float currentSpeed;
 	private Rigidbody rb;
+
 	// Use this for initialization
 	void Start () {
 		rb = GetComponent<Rigidbody> ();
-		currentSpeed = 0;
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-		float moveHorizontal = Input.GetAxis ("Horizonatal");
+		float moveHorizontal = Input.GetAxis ("Horizontal");
 		float moveVertical = Input.GetAxis ("Vertical");
 
 		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
-	
-		rb.AddForce (movement * 10);
+
+		rb.AddForce (movement * maxSpeed);
+
+		rb.rotation = Quaternion.Euler (0.0f, 0.0f, rb.velocity.x * -tilt);
 	}
 }
