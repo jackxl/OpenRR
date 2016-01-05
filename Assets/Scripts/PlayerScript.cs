7c4330abc21d391b0ce61df7f9006a6771aa0b35
pascalh <paahuijs@avans.nl>
2016-01-05 12:23:47 +0100

    add movement

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -3,16 +3,24 @@ using System.Collections;
 
 public class PlayerScript : MonoBehaviour {
 
-	public float maxSpeed;
+	public float maxSpeedKMH;
+	public float speedIncresement; 
 
 	private float currentSpeed;
+	private Rigidbody rb;
 	// Use this for initialization
 	void Start () {
-	
+		rb = GetComponent<Rigidbody> ();
+		currentSpeed = 0;
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void FixedUpdate () {
+		float moveHorizontal = Input.GetAxis ("Horizonatal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
 	
+		rb.AddForce (movement * 10);
 	}
 }
